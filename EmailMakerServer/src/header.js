ee88980736956b209ea0e1b076c0fe9285de3403
Renan Araujo Caetano <araujo.skd@gmail.com>
2024-11-21 00:05:32 -0300

    atualizacao component

--- EmailMakerServer/src/header.js
+++ EmailMakerServer/src/header.js
@@ -13,8 +13,7 @@ hr {margin:0 !important}
 div, p, a, li, td {-webkit-text-size-adjust:none;}
 .inlineblock>tbody,
 .inlineblock>tbody>tr,
-.inlineblock>tbody>tr>td {display: block; width: 100%}
-    
+.inlineblock>tbody>tr>td {display: block; width: 100%}  
 @media only screen and (max-width: 600px) {
 .imgmobile{max-width: 70%}
 }
@@ -23,7 +22,7 @@ div, p, a, li, td {-webkit-text-size-adjust:none;}
 <body>
 <div style="margin:0; padding:0;" bgcolor="#ffffff">
 <table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" style="table-layout: fixed;" bgcolor="#F6F6F6"><tr><td align="center" valign="top">
-<!--[if mso]><table width="650" cellpadding="0" cellspacing="0" border="0" align="center"><tr><td align="center" valign="top"><![endif]-->
-<table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" style="max-width:650px" bgcolor="#${bgcolor}">`
+<!--[if mso]><table width="680" cellpadding="0" cellspacing="0" border="0" align="center"><tr><td align="center" valign="top"><![endif]-->
+<table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" style="max-width:680px" bgcolor="#${bgcolor}">`
 
 exports.HTMLbody1 = header;
