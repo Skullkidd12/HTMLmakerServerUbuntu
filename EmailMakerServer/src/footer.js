01a1b07b244723c5b19e2a8d232968e6a8e424d5
Renan Araujo Caetano <araujo.skd@gmail.com>
2024-11-28 02:29:19 -0300

    adicionadas vitrines 3prod

--- EmailMakerServer/src/footer.js
+++ EmailMakerServer/src/footer.js
@@ -1,5 +1,6 @@
 const footer = `</table>
 <!--[if mso]></td></tr></table><![endif]-->
+<tr><td align="center" valign="top" bgcolor="#ff6f55" style="padding: 5px"></td></tr>
 </td></tr></table>
 </div>
 </body>
