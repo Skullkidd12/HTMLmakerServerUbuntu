b53ba479365db51a0084844f38a24c011cdb7829
Renan Araujo Caetano <araujo.skd@gmail.com>
2025-06-04 22:58:39 -0300

    test log headers

--- EmailMakerServer/src/app.js
+++ EmailMakerServer/src/app.js
@@ -187,6 +187,7 @@ app.post("/MakeHTML", (req, res) => {
         }
     );
     console.log('disponibilizou download');
+    console.log('Headers:', res.header)
 });
 
 //escreve o arquivo template njk baseado nos inputs do forms e joga pro preview
